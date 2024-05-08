e60996337a4531138300747714d8cf235e6aa989
MrAndreyOfficial <mrandreyofficial@gmail.com>
2024-05-08 23:46:38 +0500

    List Of Names

---
+++ WPF/Names/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+
+namespace Names
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNameToList(object sender, RoutedEventArgs e)
+        {
+            string name = nameBox.Text;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Name box is empty");
+                return;
+            }
+
+            bool canAdd = !(string.IsNullOrWhiteSpace(name) || listOfNames.Items.Contains(name));
+
+            if (!canAdd)
+            {
+                MessageBox.Show("This name is on the list");
+                return;
+            }
+
+            listOfNames.Items.Add(name);
+            nameBox.Clear();
+        }
+
+        private void RemoveNameFromList(object sender, RoutedEventArgs e)
+        {
+            object name = listOfNames.SelectedItem;
+
+            bool canRemove = name != null;
+
+            if (!canRemove)
+            {
+                MessageBox.Show("Select name");
+                return;
+            }
+
+            listOfNames.Items.Remove(name);
+        }
+    }
+}
---
+++ WPF/Names/Readme.md
@@ -0,0 +1,7 @@
+# Screenshots
+![main_window](./Screenshots/main_window.png)
+![names_are_on_the_list](./Screenshots/names_are_on_the_list.png)
+
+## Used technologies
+[C#](https://dotnet.microsoft.com/en-us/languages/csharp)
+[WPF](https://learn.microsoft.com/en-us/dotnet/desktop/wpf/overview)
